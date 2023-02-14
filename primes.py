e0ab38955f96150b8e5e8b89fb84e5c427fa6a44
Alejandra Alfonso <maria.alfaro@correounivalle.edu.co>
2023-02-14 15:23:54 -0500

    to determine whether an integer number is prime or not, and the dividers it has

---
+++ primes.py
@@ -0,0 +1,37 @@
+# Lista los divisores de un numero
+def Divisores(num):
+	lista = []
+	for i in range(1,num+1):
+		if num%i == 0:
+			lista.append(i)
+		else:
+			continue
+	return lista
+
+# Determina si un numero es primo
+def PI(n):
+	out = True
+	div = Divisores(n)
+	if len(div) != 2:
+		out = False
+	return out 
+
+# -----------------------------------------------
+while True:
+	try: 
+		entry = int(input("\nWrite an integer number: "))
+		if entry < 0:
+			entry *= -1
+			print("\nThe number was changed to", entry)
+		if entry == 0:
+			print("\nZero is not a prime number.\n")
+			break
+		if entry == 1:
+			print("\nOne is not a prime number.\n")
+			break
+		print("\n --> Is {} prime?".format(entry), PI(entry))
+		print(" --> Positive divisors:", Divisores(entry))
+		print()
+		break
+	except:
+		print("\nSomething's wrong. Try again.")
